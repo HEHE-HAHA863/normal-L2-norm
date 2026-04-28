8cf137debdfa2a77ecec68c4a370e4cd5e23d717
Yicheng Xu <xuyicheng@stu.sufe.edu.cn>
2026-04-28 23:15:55 +0800

    进化，日至文件也要填上参数，这样方便知道，哪个实验是用什么环境跑的

--- run_exp.sh
+++ run_exp.sh
@@ -20,6 +20,7 @@ EXPERIMENT=""
 MMD_FEATURE_NORMALIZATION="--normalize_mmd_features"
 MMD_KERNEL_SIGMA_SCALES="1.0"
 # MMD_KERNEL_SIGMA_SCALES="1.0,0.5,0.2,0.1"
+MMD_KERNEL_LABEL="gaussian_rbf_l2"
 
 set -- \
   --dataset "$DATASET" \
@@ -39,4 +40,13 @@ fi
 set -- "$@" $MMD_FEATURE_NORMALIZATION
 set -- "$@" --mmd_kernel_sigma_scales "$MMD_KERNEL_SIGMA_SCALES"
 
-python -u mmd_gan.py "$@" 2>&1 | tee "run_$(date +%Y%m%d_%H%M%S).out"
+if [ -n "$MMD_FEATURE_NORMALIZATION" ]; then
+  MMD_NORMALIZATION_LABEL="l2norm"
+else
+  MMD_NORMALIZATION_LABEL="raw"
+fi
+
+MMD_SIGMA_LABEL=$(printf "%s" "$MMD_KERNEL_SIGMA_SCALES" | sed 's/[[:space:]]//g; s/\./p/g; s/,/_/g')
+LOG_FILE="run_kernel-${MMD_KERNEL_LABEL}_sigma-${MMD_SIGMA_LABEL}_features-${MMD_NORMALIZATION_LABEL}_$(date +%Y%m%d_%H%M%S).out"
+
+python -u mmd_gan.py "$@" 2>&1 | tee "$LOG_FILE"
